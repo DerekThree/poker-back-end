318cd76e5bd48ac9be611a3b2744afc68aeeb247
Derek Ochal <derek.ochal@westernunion.com>
2024-09-29 19:37:49 -0500

    Implemented Camel

--- poker/src/main/java/games/poker/controller/PokerController.java
+++ poker/src/main/java/games/poker/controller/PokerController.java
@@ -1,19 +1,36 @@
 package games.poker.controller;
 
+import org.apache.camel.CamelExecutionException;
+import org.apache.camel.ProducerTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/v1/poker")
 public class PokerController {
 
+    @Autowired
+    private ProducerTemplate producerTemplate;
+
     @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/health")
     public ResponseEntity<String> statusCheck() {
-        return ResponseEntity.ok("Back end is UP");
+
+        Map<String, String> inputDto = new HashMap<String, String>();
+        try {
+            producerTemplate.sendBody("direct:inputData", inputDto);
+        } catch (CamelExecutionException ex) {
+            System.out.println(ex.getExchange().getException().getMessage());
+        }
+
+        return ResponseEntity.ok(inputDto.get("key"));
     }
 }
